24cddcac64fe6915718451b3b472d31f45b822d9
Kostya-tuk <klyshnikovkostya@yandex.ru>
2026-07-16 16:28:06 +0500

    Update config.js2423

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
+// Конфигурация Supabase
 const SUPABASE_URL = 'https://ziiqhrbkjqxdfmicbnpe.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_Z8jumEDG_YyYmjmRQFO3lA_chlpLvUM';
 
+// Инициализация клиента Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
