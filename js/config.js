d993a132ffb4e77d6463f53c06664e26f83ef10f
Kostya-tuk <klyshnikovkostya@yandex.ru>
2026-07-16 17:34:12 +0500

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Конфигурация Supabase
-const SUPABASE_URL = 'https://ziiqhrbkjqxdfmicbnpe.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_Z8jumEDG_YyYmjmRQFO3lA_chlpLvUM';
-
-// Инициализация клиента (используем глобальный supabase из UMD-сборки)
-const dbClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
